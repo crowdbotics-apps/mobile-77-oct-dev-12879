5d1f1b4789a5376c55b6cbce499de902193b82c2
Crowdbotics <team@crowdbotics.com>
2020-10-07 11:48:08 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -14,6 +14,14 @@ class CustomText(models.Model):
         null=True,
         blank=True,
     )
+    hfhgfhf = models.BigIntegerField(
+        null=True,
+        blank=True,
+    )
+    ghfhgf = models.BigIntegerField(
+        null=True,
+        blank=True,
+    )
 
     def __str__(self):
         return self.title
